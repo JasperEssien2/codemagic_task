6f549d479f578ee62d76c40e029e00663a684789
JasperEssien2 <jasperessien2@gmail.com>
2022-02-24 14:52:44 +0100

    implemented author_service file, and author models

--- lib/services/author_models.dart
+++ lib/services/author_models.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class Author {
   Author({
     this.link,
@@ -18,6 +20,46 @@ class Author {
   final String? quoteCount;
   final String? slug;
   final String? image;
+
+  factory Author.fromMap(Map<String, dynamic> map, String image) {
+    return Author(
+      link: map['link'],
+      bio: map['bio'],
+      description: map['description'],
+      id: map['id'],
+      name: map['name'],
+      quoteCount: map['quoteCount'],
+      slug: map['slug'],
+      image: image,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Author &&
+        other.link == link &&
+        other.bio == bio &&
+        other.description == description &&
+        other.id == id &&
+        other.name == name &&
+        other.quoteCount == quoteCount &&
+        other.slug == slug &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return link.hashCode ^
+        bio.hashCode ^
+        description.hashCode ^
+        id.hashCode ^
+        name.hashCode ^
+        quoteCount.hashCode ^
+        slug.hashCode ^
+        image.hashCode;
+  }
 }
 
 class AuthorList {
@@ -36,4 +78,45 @@ class AuthorList {
   final int? page;
   final int? totalPages;
   final int? lastItemIndex;
+
+  factory AuthorList.fromMap(
+      Map<String, dynamic> map, String Function(String slug) imageUrlBuilder) {
+    return AuthorList(
+      authors: map['authors'] != null
+          ? List<Author>.from(
+              map['authors']?.map(
+                (x) => Author.fromMap(x, imageUrlBuilder(x['slug'])),
+              ),
+            )
+          : null,
+      count: map['count']?.toInt(),
+      totalCount: map['totalCount']?.toInt(),
+      page: map['page']?.toInt(),
+      totalPages: map['totalPages']?.toInt(),
+      lastItemIndex: map['lastItemIndex']?.toInt(),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is AuthorList &&
+      listEquals(other.authors, authors) &&
+      other.count == count &&
+      other.totalCount == totalCount &&
+      other.page == page &&
+      other.totalPages == totalPages &&
+      other.lastItemIndex == lastItemIndex;
+  }
+
+  @override
+  int get hashCode {
+    return authors.hashCode ^
+      count.hashCode ^
+      totalCount.hashCode ^
+      page.hashCode ^
+      totalPages.hashCode ^
+      lastItemIndex.hashCode;
+  }
 }
--- lib/services/author_service.dart
+++ lib/services/author_service.dart
@@ -1,11 +1,24 @@
 import 'package:codemagic_task/services/author_models.dart';
+import 'package:dio/dio.dart';
+import 'package:either_dart/either.dart';
 
 class AuthorService {
   final String authorUrl = "https://quotable.io/authors";
+  final dioInstance = Dio();
 
-  Future<AuthorList> fetchAuthors() async {
-    return AuthorList();
+  Future<Either<AuthorList, String>> fetchAuthors({int page = 1}) async {
+    try {
+      final response = await dioInstance.get(
+        authorUrl,
+        queryParameters: {'page': page},
+      );
+
+      return Left(AuthorList.fromMap(response.data, authorImage));
+    } catch (e) {
+      return const Right("An error occurred, please try again!");
+    }
   }
 
-  String authorImage(String id) => '';
+  String authorImage(String slug) =>
+      "https://images.quotable.dev/profile/400/$slug.jpg";
 }
