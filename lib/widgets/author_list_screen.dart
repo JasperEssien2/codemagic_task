a1c213a340efda3828cb4dd4e2bf9eaf24298aba
JasperEssien2 <jasperessien2@gmail.com>
2022-02-24 18:10:44 +0100

    set up transition from list to detail page to use transitions

--- lib/widgets/author_list_screen.dart
+++ lib/widgets/author_list_screen.dart
@@ -13,9 +13,30 @@ class AuthorsListScreen extends StatefulWidget {
 
 class _AuthorsListScreenState extends State<AuthorsListScreen> {
   final authors = [
-    Author(name: "Jahswill", image: '', description: 'My description'),
-    Author(name: "Essien", image: '', description: 'My description'),
-    Author(name: "Samuel", image: '', description: 'My description'),
+    Author(
+      slug: 'slug1',
+      image:
+          "https://www.gannett-cdn.com/media/USATODAY/USATODAY/2013/03/22/ap-obit-achebe-16_9.jpg?width=2164&height=1223&fit=crop&format=pjpg&auto=webp",
+      name: "Jasper Essien",
+      description: "A description",
+      bio: """What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      """,
+    ),
+    Author(
+      name: "Essien",
+      image: '',
+      description: 'My description',
+      slug: 'slug2',
+      bio: "A very short bio",
+    ),
+    Author(
+      name: "Samuel",
+      image: '',
+      description: 'My description',
+      slug: 'slug3',
+      bio: "A very very short bio",
+    ),
   ];
 
   @override
