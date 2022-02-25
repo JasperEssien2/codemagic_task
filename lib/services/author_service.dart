f3834aeac67662a212dc6590559c1c9ced3fe039
JasperEssien2 <jasperessien2@gmail.com>
2022-02-25 15:12:11 +0100

    refactored service class to follow dependency inversion principle

--- lib/services/author_service.dart
+++ lib/services/author_service.dart
@@ -5,8 +5,13 @@ import 'package:dio/dio.dart';
 import 'package:either_dart/either.dart';
 
 class AuthorService {
-  final String authorUrl = "https://quotable.io/authors";
-  final dioInstance = Dio();
+  AuthorService({
+    required this.authorUrl,
+    required this.dioInstance,
+  });
+
+  final String authorUrl;
+  final Dio dioInstance;
 
   Future<Either<AuthorList, String>> fetchAuthors({int page = 1}) async {
     try {
