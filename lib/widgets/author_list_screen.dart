327f15269408682d6ead2b1c061cf19e8f7bf0b7
JasperEssien2 <jasperessien2@gmail.com>
2022-02-25 06:40:40 +0100

    implementation in progress

--- lib/widgets/author_list_screen.dart
+++ lib/widgets/author_list_screen.dart
@@ -1,4 +1,3 @@
-import 'package:codemagic_task/services/author_models.dart';
 import 'package:codemagic_task/state_management/app_state.dart';
 import 'package:codemagic_task/widgets/appbar.dart';
 import 'package:flutter/material.dart';
@@ -13,36 +12,10 @@ class AuthorsListScreen extends StatefulWidget {
 }
 
 class _AuthorsListScreenState extends State<AuthorsListScreen> {
-  final authors = [
-    Author(
-      slug: 'slug1',
-      image:
-          "https://www.gannett-cdn.com/media/USATODAY/USATODAY/2013/03/22/ap-obit-achebe-16_9.jpg?width=2164&height=1223&fit=crop&format=pjpg&auto=webp",
-      name: "Jasper Essien",
-      description: "A description",
-      bio: """What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      """,
-    ),
-    Author(
-      name: "Essien",
-      image: '',
-      description: 'My description',
-      slug: 'slug2',
-      bio: "A very short bio",
-    ),
-    Author(
-      name: "Samuel",
-      image: '',
-      description: 'My description',
-      slug: 'slug3',
-      bio: "A very very short bio",
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     final appState = AppState.of(context);
+    final authors = appState.logic.authorList;
 
     return Scaffold(
       appBar: PreferredSize(
@@ -59,8 +32,8 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
       ),
       body: SafeArea(
         child: FutureBuilder(
-          // future: appState.,
-          builder: (c, state)=> ListView.builder(
+          future: appState.logic.fetchAuthors(),
+          builder: (c, state) => ListView.builder(
             itemCount: authors.length,
             padding: const EdgeInsets.symmetric(vertical: 24),
             physics: const BouncingScrollPhysics(),
