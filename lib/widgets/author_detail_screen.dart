8d31640b44efe9c53ea5b7ea6894beae0cf85f86
JasperEssien2 <jasperessien2@gmail.com>
2022-02-28 13:12:03 +0100

    ..

--- lib/widgets/author_detail_screen.dart
+++ lib/widgets/author_detail_screen.dart
@@ -16,7 +16,6 @@ class _AuthorDetailScreenState extends State<AuthorDetailScreen> {
   @override
   void didChangeDependencies() {
     _author = ModalRoute.of(context)!.settings.arguments as Author;
-    
     super.didChangeDependencies();
   }
 
